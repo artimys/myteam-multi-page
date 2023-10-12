8d5c84415bdabd1ab7151707deb8873a98658e02
Arturo Simon <artysimon@gmail.com>
2023-10-11 23:14:22 -0400

    Add sticky navbar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,45 @@
+"use client";
+
+import { useRef, useEffect } from "react";
 import Link from "next/link";
 import FullBleedContainer from "@/components/layout/FullBleedContainer";
 import MyTeamLogo from "@/components/svg/MyTeamLogo";
 import SlideOutMenu from "@/components/slide-out-menu/SlideOutMenu";
 
 function Header() {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const headerElement = headerRef.current;
+
+      if (!headerElement) {
+        return;
+      }
+      if (prevScrollPos > currentScrollPos) {
+        headerElement.style.transform = "translateY(0)";
+      } else {
+        headerElement.style.transform = "translateY(-216px)";
+      }
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <FullBleedContainer classNames="bg-primary-midnight-green">
-      <header className="container pt-10 pb-20 md:pb-28 md:pt-16">
+      <header
+        className="container fixed top-0 left-0 right-0 z-50 pt-10 transition-transform duration-300 transform translate-y-0 xxxpb-20 xxmd:pb-28 md:pb-10 md:pt-16 bg-primary-midnight-green"
+        ref={headerRef}
+      >
         <div className="flex flow-row max-w-[1110px] mx-auto">
           {/* TODO sm md 128px, lg xl 160 */}
           <Link href="/" className="mr-20">
