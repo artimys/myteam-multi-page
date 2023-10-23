3aff3329309eb65f040c1d82acbd45b8ead67515
Arturo Simon <artysimon@gmail.com>
2023-10-22 23:44:47 -0400

    Animate skip to content link

--- src/components/layout/SkipToMainContent.tsx
+++ src/components/layout/SkipToMainContent.tsx
@@ -5,8 +5,9 @@ function SkipToMainContent() {
     <Link
       href="#main-content"
       aria-label="Skip to main content"
-      className="absolute block py-3 mx-auto mt-5 font-bold text-center rounded-full w-60 -left-full border-secondary-deep-jungle-green bg-primary-light-coral focus:left-0 focus:right-0 text-secondary-sacramento-state-green hover:underline"
-      tabIndex={0}
+      // className="absolute left-0 right-0 block py-3 mx-auto font-bold text-center -translate-y-[150%] transition-[transform] duration-300 ease-in-out rounded-full w-60 border-secondary-deep-jungle-green bg-primary-light-coral text-secondary-sacramento-state-green hover:underline focus:translate-y-1/2"
+      className="btn btn-primary--dark absolute left-0 right-0 block mx-auto -translate-y-[150%] transition-[transform] duration-300 ease-in-out w-60 focus:translate-y-1/2 bg-primary-light-coral text-center"
+      tabIndex={1}
     >
       Skip to main content
     </Link>
