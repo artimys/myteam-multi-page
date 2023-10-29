b97a3a67509a91181902c7334cecea9d51a582c5
Arturo Simon <artysimon@gmail.com>
2023-10-28 23:59:09 -0400

    Fix Ready to get started heading tag

--- src/components/layout/ReadyGetStarted.jsx
+++ src/components/layout/ReadyGetStarted.jsx
@@ -6,9 +6,9 @@ function ReadyGetStarted() {
     <FullBleedContainer classNames="bg-pattern-get-started bg-primary-light-coral">
       <div className="container">
         <div className="py-16 text-center xl:py-16 md:flex xl:items-center md:justify-between max-w-xl xl:max-w-[917px] mx-auto">
-          <h4 className="text-[32px] xl:text-5xl font-bold px-16 leading-8 md:px-0">
+          <h3 className="text-[32px] xl:text-5xl font-bold px-16 leading-8 md:px-0">
             Ready to get started?
-          </h4>
+          </h3>
 
           <Link href="/contact" className="mt-5 btn btn-primary--dark md:mt-0">
             contact us
